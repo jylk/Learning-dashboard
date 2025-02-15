0be731db429baa04eb47a6ad62a90a0eefce1979
Jaya lakshmi <rjayalakshmi901@gmail.com>
2025-02-15 12:55:56 +0530

    s

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async() =>{
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect('mongodb+srv://rjayalakshmi901:7SILqBgQcsl6PPXM@cluster0.mmmaj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/Learning-Dashboard');
         console.log("db connected sucessfully")
     } catch (error) {
         console.log(error);
